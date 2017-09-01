88b78e098d55469ce1a0471cecae5b4f6b1569da
Gangadhar <gangadhar6910@gmail.com>
2017-09-01 07:53:38 +0530

    Sample Code to found Repeated Numbers in a set of Numbers

---
+++ src/CountRepeatedNum.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class CountRepeatedNum
+{
+	HashMap<Integer,Integer> dmap=new HashMap<Integer,Integer>();
+	public void findDupOcc(ArrayList aL)
+	{
+		for(int i=0;i<aL.size();i++)
+		{
+			int val=(int) aL.get(i);
+			if(dmap!=null&&!dmap.equals("")&&dmap.containsKey(val))
+			{
+				dmap.put(val, dmap.get(val)+1);
+			}
+			else
+			{
+				dmap.put(val,1);
+			}
+		}
+		
+		for(Map.Entry<Integer,Integer> entry:dmap.entrySet())
+		{
+			System.out.println("The Value"+entry.getKey()+"is repeated for"+entry.getValue()+"Times");
+			
+		}
+		
+	}
+	
+
+	public static void main(String[] args) 
+	{
+		
+		ArrayList<Integer> aList=new ArrayList<Integer>();
+		aList.add(10);
+		aList.add(5);aList.add(10);aList.add(8);aList.add(4);aList.add(8);aList.add(10);
+		CountRepeatedNum crn=new CountRepeatedNum();
+		crn.findDupOcc(aList);
+		
+	}
+
+}
